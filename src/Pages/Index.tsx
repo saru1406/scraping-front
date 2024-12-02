613ee16f357f527c88bab3259b8a73656583278f
yuta-saruwatari <y.s1406@ezweb.ne.jp>
2024-12-03 00:08:09 +0900

    微修正

--- src/Pages/Index.tsx
+++ src/Pages/Index.tsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 
 type Job = {
     id: number;
@@ -15,7 +16,7 @@ function Index() {
 
     useEffect(() => {
         const fetchJobs = async () => {
-            const url = 'http://localhost:80/jobs';
+            const url = import.meta.env.VITE_API_BASE_URL+'/jobs';
             try {
                 const response = await fetch(url, {
                     method: 'GET'
@@ -33,39 +34,37 @@ function Index() {
 
         fetchJobs();
     }, []);
-
-    console.log(jobs)
-
-    return(
+    return (
         <section className="text-gray-600 body-font">
+        <Link to="/" className="m-28 hover:text-gray-300 text-xl">戻る</Link>
+
             <div className="container px-5 py-24 mx-auto">
                 <div className="flex flex-col text-center w-full mb-20">
                     <h1 className="sm:text-4xl text-3xl font-medium title-font mb-2 text-gray-900">案件一覧</h1>
                     {/* <p className="lg:w-2/3 mx-auto leading-relaxed text-base">Banh mi cornhole echo park skateboard authentic crucifix neutra tilde lyft biodiesel artisan direct trade mumblecore 3 wolf moon twee</p> */}
                 </div>
-                <div className="lg:w-11/12 w-full mx-auto overflow-auto">
-                    <table className="table-auto w-full text-left whitespace-no-wrap">
+                <div className="lg:w-11/12 w-full mx-auto">
+                    <table className="table-fixed w-full text-left whitespace-no-wrap">
                         <thead>
                             <tr>
-                                <th className="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100 rounded-tl rounded-bl">タイトル</th>
-                                <th className="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">詳細</th>
-                                <th className="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">金額</th>
-                                <th className="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100  rounded-tr rounded-br text-nowrap">求人数</th>
+                                <th className="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100 rounded-tl rounded-bl w-1/4">タイトル</th>
+                                <th className="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100 w-1/2">詳細</th>
+                                <th className="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100 w-1/6">金額</th>
+                                <th className="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100 rounded-tr rounded-br w-1/6 text-nowrap">求人数</th>
                             </tr>
                         </thead>
                         <tbody>
-                            {jobs.map(job => 
+                            {jobs.map(job => (
                                 <tr key={job.id} onClick={() => window.open(job.link, '_blank')} style={{ cursor: 'pointer' }}>
                                     <td className="px-4 py-3 border">{job.title}</td>
-                                    <td className="px-4 py-3  border">{job.show}</td>
-                                    <td className="px-4 py-3  border">{job.price}</td>
-                                    <td className="px-4 py-3  border">{job.limit}</td>
+                                    <td className="px-4 py-3 border">{job.show}</td>
+                                    <td className="px-4 py-3 border">{job.price}</td>
+                                    <td className="px-4 py-3 border">{job.limit}</td>
                                 </tr>
-                            )}
+                            ))}
                         </tbody>
                     </table>
                 </div>
-
             </div>
         </section>
     )
--- src/Pages/chat.tsx
+++ src/Pages/chat.tsx
@@ -4,31 +4,29 @@ import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
 import { coy } from 'react-syntax-highlighter/dist/esm/styles/prism';
 import remarkGfm from 'remark-gfm';
 import remarkBreaks from 'remark-breaks';
+import { Link } from "react-router-dom";
 
 type Chat = {
     text: string;
-    sender: 'user' | 'bot';
+    role: 'user' | 'assistant';
 };
 
 const ChatPage = () => {
     const [chat, setChat] = useState('');
     const [chatHistory, setChatHistory] = useState<Chat[]>([]);
-    const [displayedText, setDisplayedText] = useState('');
+    console.log(chatHistory)
 
     useEffect(() => {
         if (chatHistory.length > 0) {
             let index = 0;
             let messageIndex = 0;
-            setDisplayedText('');
             const interval = setInterval(() => {
                 if (messageIndex < chatHistory.length) {
                     if (index < chatHistory[messageIndex].text.length) {
-                        setDisplayedText(prev => prev + chatHistory[messageIndex].text.charAt(index));
                         index++;
                     } else {
                         index = 0;
                         messageIndex++;
-                        setDisplayedText(prev => prev + '\n');
                     }
                 } else {
                     clearInterval(interval);
@@ -37,26 +35,27 @@ const ChatPage = () => {
             return () => clearInterval(interval);
         }
     }, [chatHistory]);
-    
 
     const postChat = async (event: React.FormEvent<HTMLFormElement>) => {
         event.preventDefault();
-        const url = 'http://localhost:80/prompts';
+        const url = import.meta.env.VITE_API_BASE_URL+'/prompts';
         try {
-            const userMessage: Chat = { text: chat, sender: 'user' };
-            setChatHistory(prev => [...prev, userMessage]);
+            const userMessage: Chat = { text: chat, role: 'user' };
+            const updatedHistory = [...chatHistory, userMessage];
+            setChatHistory(updatedHistory);
+            setChat('');
             const response = await fetch(url, {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
                 },
-                body: JSON.stringify({ text: chat }),
+                body: JSON.stringify({ text: updatedHistory }),
             });
             if (response.ok) {
+                // サーバーからの返信を履歴に追加
                 const data = await response.json();
-                const botMessage: Chat = { text: data.text, sender: 'bot' };
+                const botMessage: Chat = { text: data.text, role: 'assistant' };
                 setChatHistory(prev => [...prev, botMessage]);
-                setDisplayedText('');
                 setChat('');
             } else {
                 console.error('エラーが発生しました');
@@ -67,62 +66,81 @@ const ChatPage = () => {
     };
 
     return (
-        <div className="flex flex-col h-screen">
-            <div className="flex flex-1">
-                <div className="w-64 bg-gray-800 text-white p-4">
-                    <h2 className="text-xl font-bold mb-4">Sidebar</h2>
-                    <ul>
-                        <li className="mb-2"><a href="#" className="hover:text-gray-300">Home</a></li>
-                        <li className="mb-2"><a href="#" className="hover:text-gray-300">Profile</a></li>
-                        <li className="mb-2"><a href="#" className="hover:text-gray-300">Settings</a></li>
-                    </ul>
-                </div>
-                <div className="flex-1 p-4 flex flex-col">
-                    <div className="container mx-auto mb-4 flex-1 overflow-y-auto">
-                        <h1 className="text-2xl font-bold mb-4">Welcome to the Chat Page</h1>
-                        {chatHistory.map((message, index) => (
-                            <div
-                                key={index}
-                                className={`mt-2 mb-2 p-2 rounded-lg ${message.sender === 'user' ? 'bg-blue-200 text-black ml-auto w-72' : 'bg-gray-100 text-black self-start'}`}
-                                style={{ maxWidth: '80%', whiteSpace: 'pre-wrap' }}
+        <div className="flex">
+            {/* サイドバー */}
+            <div className="w-64 bg-gray-800 text-white p-4 h-screen fixed">
+                <h2 className="text-xl font-bold mb-10">RagChat</h2>
+                <ul>
+                    <li className="mb-8">
+                        <Link to="/" className="hover:text-gray-300 text-xl">案件検索</Link>
+                    </li>
+                    {/* <li className="mb-10">
+                        <Link to="/custom" className="hover:text-gray-300 text-xl">カスタム</Link>
+                    </li> */}
+                </ul>
+            </div>
+
+            {/* メインコンテンツ */}
+            <div className="flex-1 flex flex-col ml-64 p-4 h-screen">
+                {/* チャット履歴 */}
+                <div className="flex-1 overflow-y-auto">
+                    <h1 className="text-2xl font-bold mb-4">案件検索</h1>
+                    {chatHistory.map((message, index) => (
+                        <div
+                            key={index}
+                            className={`mt-2 mb-2 p-2 rounded-lg ${message.role === 'user' ? 'bg-blue-200 text-black ml-auto w-72' : 'bg-gray-100 text-black self-start'}`}
+                            style={{ maxWidth: '80%', whiteSpace: 'pre-wrap' }}
+                        >
+                            <ReactMarkdown
+                                remarkPlugins={[remarkGfm, remarkBreaks]}
+                                components={{
+                                    code({
+                                        inline,
+                                        className,
+                                        children,
+                                        ...props
+                                    }: {
+                                        inline?: boolean;
+                                        className?: string;
+                                        children?: React.ReactNode;
+                                    }) {
+                                        const match = /language-(\w+)/.exec(className || '');
+                                        return !inline && match ? (
+                                            <SyntaxHighlighter
+                                                style={coy}
+                                                language={match[1]}
+                                                PreTag="div"
+                                                {...props}
+                                            >
+                                                {String(children).replace(/\n$/, '')}
+                                            </SyntaxHighlighter>
+                                        ) : (
+                                            <code className={className} {...props}>
+                                                {children}
+                                            </code>
+                                        );
+                                    },
+                                }}            
                             >
-                                <ReactMarkdown
-                                    remarkPlugins={[remarkGfm, remarkBreaks]}
-                                    components={{
-                                        code({ node, inline, className, children, ...props }) {
-                                            const match = /language-(\w+)/.exec(className || '');
-                                            return !inline && match ? (
-                                                <SyntaxHighlighter
-                                                    style={coy}
-                                                    language={match[1]}
-                                                    PreTag="div"
-                                                    {...props}
-                                                >
-                                                    {String(children).replace(/\n$/, '')}
-                                                </SyntaxHighlighter>
-                                            ) : (
-                                                <code className={className} {...props}>
-                                                    {children}
-                                                </code>
-                                            );
-                                        },
-                                    }}
-                                >
-                                    {message.text}
-                                </ReactMarkdown>
-                            </div>
-                        ))}
-                    </div>
-                    <form className="flex" onSubmit={postChat}>
-                        <input
-                            type="text"
-                            className="flex-1 p-2 border border-gray-400 rounded mr-2"
-                            placeholder="Type your message..."
-                            value={chat}
-                            onChange={(e) => setChat(e.target.value)}
-                        />
-                        <button type="submit" className="bg-blue-500 text-white p-2 rounded">Send</button>
-                    </form>
+                                {message.text}
+                            </ReactMarkdown>
+                        </div>
+                    ))}
+                </div>
+
+                {/* 入力フォーム */}
+                <form className="flex mt-auto" onSubmit={postChat}>
+                    <input
+                        type="text"
+                        className="flex-1 p-2 border border-gray-400 rounded mr-2"
+                        placeholder="メッセージを入力してください"
+                        value={chat}
+                        onChange={(e) => setChat(e.target.value)}
+                    />
+                    <button type="submit" className="bg-blue-500 text-white p-2 rounded">送信</button>
+                </form>
+                <div>
+                    <Link to="/index" className="text-blue-500 hover:text-blue-700">登録データ確認</Link>
                 </div>
             </div>
         </div>
